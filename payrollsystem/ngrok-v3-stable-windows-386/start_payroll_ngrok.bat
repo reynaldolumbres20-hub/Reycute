00a2be02a848f37193d113b0b955e2dc6f7526ba
Reynaldo B. Lumbres <reynaldolumbres20@gmail.com>
2026-05-31 10:10:39 +0800

    Update database.php for Railway and remove ngrok folder

--- payrollsystem/ngrok-v3-stable-windows-386/start_payroll_ngrok.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Pumunta sa folder ng ngrok
-cd /d D:\OneDrive\Documents\Desktop\bosing\ngrok-v3-stable-windows-386
-
-REM Start ngrok sa port 80
-start ngrok.exe http 80
-
-REM Optional: Open default browser sa login page
-start "" "https://stereotyped-kingsley-unwrongfully.ngrok-free.dev/payrollsystem/login.php"
